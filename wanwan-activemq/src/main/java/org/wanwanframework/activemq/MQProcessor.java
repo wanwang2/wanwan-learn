a70a572ad81075bdfb4dbf87528e4f1a99ba740b
[wanwang2] <632468635@qq.com>
2016-11-17 15:08:32 +0800

    add activemq

---
+++ wanwan-activemq/src/main/java/org/wanwanframework/activemq/MQProcessor.java
@@ -0,0 +1,32 @@
+package org.wanwanframework.activemq;
+
+import com.chinamobile.iot.udm.jms.JMSConnector;
+
+public class MQProcessor {
+
+	private JMSConnector jmsConnector;
+	private String message;
+	
+	public void send() {
+		jmsConnector.send(message);
+		System.out.println("message:" + message);
+	}
+
+	public JMSConnector getJmsConnector() {
+		return jmsConnector;
+	}
+
+	public void setJmsConnector(JMSConnector jmsConnector) {
+		this.jmsConnector = jmsConnector;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
