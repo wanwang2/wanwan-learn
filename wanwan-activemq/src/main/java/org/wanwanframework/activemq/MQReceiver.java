8b10d8bac58b9415cf6b12f97407efc415b73622
[wanwang2] <632468635@qq.com>
2016-11-18 15:07:08 +0800

    the last commit

--- wanwan-activemq/src/main/java/org/wanwanframework/activemq/MQReceiver.java
+++ wanwan-activemq/src/main/java/org/wanwanframework/activemq/MQReceiver.java
@@ -5,10 +5,13 @@ import org.apache.camel.Processor;
 
 public class MQReceiver implements Processor{
 
+	private int count = 0;
+	
 	public void process(Exchange exchange) throws Exception {
 		String messge = exchange.getIn().getBody().toString();
-		System.out.println("message:" + messge);
-		
+		System.out.println("exchange.getIn().getBody():" + messge);
+		count++;
+		System.out.println("count:" + count);
 	}
 
 }
