a37f786d86527cf02ece331cd87bf324f109025c
[wanwang2] <632468635@qq.com>
2016-12-01 17:46:28 +0800

    add

---
+++ wanwan-spring-initialzr/src/main/java/org/wanwanframework/springinitialzr/WanwanSpringInitialzrApplication.java
@@ -0,0 +1,12 @@
+package org.wanwanframework.springinitialzr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanwanSpringInitialzrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanwanSpringInitialzrApplication.class, args);
+	}
+}
