3acb54f23400727d7227fddb0e722cf52446d11b
[wanwang2] <632468635@qq.com>
2016-12-14 11:22:59 +0800

    set avro dir

--- wanwan-avro/src/test/java/org/wanwanframework/avro/TestGenericMapping.java
+++ wanwan-avro/src/test/java/org/wanwanframework/avro/TestGenericMapping.java
@@ -1,78 +1,78 @@
-package org.wanwanframework.avro;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericData;
-import org.apache.avro.generic.GenericDatumReader;
-import org.apache.avro.generic.GenericDatumWriter;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DatumReader;
-import org.apache.avro.io.DatumWriter;
-import org.apache.avro.io.Decoder;
-import org.apache.avro.io.DecoderFactory;
-import org.apache.avro.io.Encoder;
-import org.apache.avro.io.EncoderFactory;
-import org.junit.Test;
-
-import junit.framework.Assert;
-
-/**
- * Generic方式
- * @author coco
- *
- */
-public class TestGenericMapping {
-
-	@Test
-	public void test() throws IOException {
-		// 将schema从StringPair.avsc文件中加载
-		Schema.Parser parser = new Schema.Parser();
-		Schema schema = parser.parse(getClass().getResourceAsStream("/StringPair.avsc"));
-
-		GenericRecord record = new GenericData.Record(schema); // 根据schema创建一个record示例
-		record.put("left", "L");
-		record.put("right", "R");
-
-		ByteArrayOutputStream out = writeData(schema, record);
-		GenericRecord result = readData(schema, out);
-		System.out.println("result:" + result);
-		Assert.assertEquals("L", result.get("left").toString());
-		Assert.assertEquals("R", result.get("right").toString());
-	}
-
-	private ByteArrayOutputStream writeData(Schema schema, GenericRecord record) {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		// DatumWriter可以将GenericRecord变成edncoder可以理解的类型
-		DatumWriter<GenericRecord> writer = new GenericDatumWriter<GenericRecord>(schema);
-		// encoder可以将数据写入流中，binaryEncoder第二个参数是重用的encoder，这里不重用，所用传空
-		Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
-		try {
-			writer.write(record, encoder);
-			encoder.flush();
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return out;
-	}
-
-	/**
-	 * 获取Record
-	 *
-	 * @param schema
-	 * @param out
-	 * @return
-	 */
-	private GenericRecord readData(Schema schema, ByteArrayOutputStream out) {
-		DatumReader<GenericRecord> reader = new GenericDatumReader<GenericRecord>(schema);
-		Decoder decoder = DecoderFactory.get().binaryDecoder(out.toByteArray(), null);
-		try {
-			return reader.read(null, decoder);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-}
+//package org.wanwanframework.avro;
+//
+//import java.io.ByteArrayOutputStream;
+//import java.io.IOException;
+//
+//import org.apache.avro.Schema;
+//import org.apache.avro.generic.GenericData;
+//import org.apache.avro.generic.GenericDatumReader;
+//import org.apache.avro.generic.GenericDatumWriter;
+//import org.apache.avro.generic.GenericRecord;
+//import org.apache.avro.io.DatumReader;
+//import org.apache.avro.io.DatumWriter;
+//import org.apache.avro.io.Decoder;
+//import org.apache.avro.io.DecoderFactory;
+//import org.apache.avro.io.Encoder;
+//import org.apache.avro.io.EncoderFactory;
+//import org.junit.Test;
+//
+//import junit.framework.Assert;
+//
+///**
+// * Generic方式
+// * @author coco
+// *
+// */
+//public class TestGenericMapping {
+//
+//	@Test
+//	public void test() throws IOException {
+//		// 将schema从StringPair.avsc文件中加载
+//		Schema.Parser parser = new Schema.Parser();
+//		Schema schema = parser.parse(getClass().getResourceAsStream("/StringPair.avsc"));
+//
+//		GenericRecord record = new GenericData.Record(schema); // 根据schema创建一个record示例
+//		record.put("left", "L");
+//		record.put("right", "R");
+//
+//		ByteArrayOutputStream out = writeData(schema, record);
+//		GenericRecord result = readData(schema, out);
+//		System.out.println("result:" + result);
+//		Assert.assertEquals("L", result.get("left").toString());
+//		Assert.assertEquals("R", result.get("right").toString());
+//	}
+//
+//	private ByteArrayOutputStream writeData(Schema schema, GenericRecord record) {
+//		ByteArrayOutputStream out = new ByteArrayOutputStream();
+//		// DatumWriter可以将GenericRecord变成edncoder可以理解的类型
+//		DatumWriter<GenericRecord> writer = new GenericDatumWriter<GenericRecord>(schema);
+//		// encoder可以将数据写入流中，binaryEncoder第二个参数是重用的encoder，这里不重用，所用传空
+//		Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+//		try {
+//			writer.write(record, encoder);
+//			encoder.flush();
+//			out.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		return out;
+//	}
+//
+//	/**
+//	 * 获取Record
+//	 *
+//	 * @param schema
+//	 * @param out
+//	 * @return
+//	 */
+//	private GenericRecord readData(Schema schema, ByteArrayOutputStream out) {
+//		DatumReader<GenericRecord> reader = new GenericDatumReader<GenericRecord>(schema);
+//		Decoder decoder = DecoderFactory.get().binaryDecoder(out.toByteArray(), null);
+//		try {
+//			return reader.read(null, decoder);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		return null;
+//	}
+//}
--- wanwan-avro/src/test/java/org/wanwanframework/avro/TestSprecificMapping.java
+++ wanwan-avro/src/test/java/org/wanwanframework/avro/TestSprecificMapping.java
@@ -1,67 +1,67 @@
-package org.wanwanframework.avro;
-
-import org.apache.avro.io.*;
-import org.apache.avro.specific.SpecificDatumReader;
-import org.apache.avro.specific.SpecificDatumWriter;
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-/**
- * Sprecific
- * @author coco
- *
- */
-public class TestSprecificMapping {
-
-	@Test
-	public void test() throws IOException {
-		// 因为已经生成StringPair的源代码，所以不再使用schema了，直接调用setter和getter即可
-		StringPair pair = new StringPair();
-		pair.setLeft("L");
-		pair.setRight("R");
-
-		ByteArrayOutputStream out = writeData(pair);
-		StringPair readPair = readData(out);
-		System.out.println("result:" + readPair);
-		Assert.assertEquals("L", readPair.getLeft().toString());
-		Assert.assertEquals("R", readPair.getRight().toString());
-	}
-	
-	/**
-	 * 写：写到一个流里面
-	 * @param pair
-	 * @return
-	 */
-	private ByteArrayOutputStream writeData(StringPair pair) {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		DatumWriter<StringPair> writer = new SpecificDatumWriter<StringPair>(StringPair.class);
-		Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
-		try {
-			writer.write(pair, encoder);
-			encoder.flush();
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return out;
-	}
-
-	/**
-	 * 读:从一个流里面读取
-	 * @param out
-	 * @return
-	 */
-	private StringPair readData(ByteArrayOutputStream out) {
-		DatumReader<StringPair> reader = new SpecificDatumReader<StringPair>(StringPair.class);
-		Decoder decoder = DecoderFactory.get().binaryDecoder(out.toByteArray(), null);
-		try {
-			return reader.read(null, decoder);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-}
+//package org.wanwanframework.avro;
+//
+//import org.apache.avro.io.*;
+//import org.apache.avro.specific.SpecificDatumReader;
+//import org.apache.avro.specific.SpecificDatumWriter;
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//import java.io.ByteArrayOutputStream;
+//import java.io.IOException;
+//
+///**
+// * Sprecific
+// * @author coco
+// *
+// */
+//public class TestSprecificMapping {
+//
+//	@Test
+//	public void test() throws IOException {
+//		// 因为已经生成StringPair的源代码，所以不再使用schema了，直接调用setter和getter即可
+//		StringPair pair = new StringPair();
+//		pair.setLeft("L");
+//		pair.setRight("R");
+//
+//		ByteArrayOutputStream out = writeData(pair);
+//		StringPair readPair = readData(out);
+//		System.out.println("result:" + readPair);
+//		Assert.assertEquals("L", readPair.getLeft().toString());
+//		Assert.assertEquals("R", readPair.getRight().toString());
+//	}
+//	
+//	/**
+//	 * 写：写到一个流里面
+//	 * @param pair
+//	 * @return
+//	 */
+//	private ByteArrayOutputStream writeData(StringPair pair) {
+//		ByteArrayOutputStream out = new ByteArrayOutputStream();
+//		DatumWriter<StringPair> writer = new SpecificDatumWriter<StringPair>(StringPair.class);
+//		Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+//		try {
+//			writer.write(pair, encoder);
+//			encoder.flush();
+//			out.close();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		return out;
+//	}
+//
+//	/**
+//	 * 读:从一个流里面读取
+//	 * @param out
+//	 * @return
+//	 */
+//	private StringPair readData(ByteArrayOutputStream out) {
+//		DatumReader<StringPair> reader = new SpecificDatumReader<StringPair>(StringPair.class);
+//		Decoder decoder = DecoderFactory.get().binaryDecoder(out.toByteArray(), null);
+//		try {
+//			return reader.read(null, decoder);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		return null;
+//	}
+//}
