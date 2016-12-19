b6ee03f6d1652e6035bcd7004143242897af930f
[wanwang2] <632468635@qq.com>
2016-12-19 14:23:19 +0800

    v

---
+++ wanwan-zextend-python/source/files/__fileWritor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''file writor
+'''
+
+# Open a file
+file = open("resource.txt", "w")
+file.write( "Python is a great language.\nYeah its great!!balabala\n")
+
+# Close opend file
+file.close() 
