e641c9ad4fc2eba50ed781950e15b6f9996eb406
[wanwang2] <632468635@qq.com>
2017-01-01 17:34:12 +0800

    vv

---
+++ wanwan-springboots/src/main/java/org/wanwanframework/springboots/wanwan_springboots/App.java
@@ -0,0 +1,13 @@
+package org.wanwanframework.springboots.wanwan_springboots;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
