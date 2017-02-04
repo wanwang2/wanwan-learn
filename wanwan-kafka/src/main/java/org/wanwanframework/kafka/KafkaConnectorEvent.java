c5a7fc5c419b6a1e37efad31e5ca199ca474d89c
[wanwang2] <632468635@qq.com>
2017-02-04 10:27:09 +0800

    vv

---
+++ wanwan-kafka/src/main/java/org/wanwanframework/kafka/KafkaConnectorEvent.java
@@ -0,0 +1,17 @@
+package org.wanwanframework.kafka;
+
+/**
+ * Created by zhaofeng on 2016/12/9.
+ */
+public class KafkaConnectorEvent {
+    public String                threadName;
+    public KafkaConnectorEventType type;
+    public Object                event;
+
+    public KafkaConnectorEvent(String threadName, KafkaConnectorEventType type, Object event) {
+        this.threadName = threadName;
+        this.type = type;
+        this.event = event;
+    }
+
+}
---
+++ wanwan-kafka/src/main/java/org/wanwanframework/kafka/KafkaConnectorEventType.java
@@ -0,0 +1,8 @@
+package org.wanwanframework.kafka;
+
+/**
+ * Created by zhaofeng on 2016/12/9.
+ */
+public enum KafkaConnectorEventType {
+    SEND, COMMIT
+}
